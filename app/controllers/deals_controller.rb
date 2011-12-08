2554df3cb20b4a2d5cae675ed93cebeaed80cd5e
Matthew Ruten <matt@ruten.ca>
2011-12-07 19:06:32 -0500

    Add updated version of sinatra file, and delete old sinatra app

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -11,6 +11,26 @@ class DealsController < ApplicationController
     doc.css('td .amabot_center > table:first').each do |table|
       deal_html += table.to_s
     end
+
+    # New Nokogiri node with isolated deal html only
+    inner_doc = Nokogiri::HTML(deal_html)
+
+    #puts inner_doc.inspect
+
+    book = {}
+    book[:cover]            = inner_doc.css('span a img')[0]
+    book[:title]            = inner_doc.css('span p b a')[0]
+    book[:description]      = inner_doc.css('table tr td div span')[2]
+
+    price_table  = inner_doc.css('table tr td div table')[1]
+    @ap1 = ap price_table.css('tr td')
+
+    #book[:price_yesterday]  = inner_doc.css('')[1]
+    #book[:price_deal]       = inner_doc.css('span p b a')[0]
+
+
+    @ap2 = ap book
+
     
     @deal = deal_html
   end
