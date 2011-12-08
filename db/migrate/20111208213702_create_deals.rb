ea83614c85a2d2a8846db2b77f4d219ceb21038f
Matthew Ruten <matt@ruten.ca>
2011-12-08 16:38:08 -0500

    Add Deal model

---
+++ db/migrate/20111208213702_create_deals.rb
@@ -0,0 +1,14 @@
+class CreateDeals < ActiveRecord::Migration
+  def change
+    create_table :deals do |t|
+      t.string :title
+      t.float :old_price
+      t.float :current_price
+      t.string :cover_img
+      t.text :description
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
