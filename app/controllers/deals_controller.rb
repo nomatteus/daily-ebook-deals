c4d6f6fb6f3cdcee8319b0d3988a116de98f493b
nomatteus <matt@matthewruten.com>
2012-01-03 04:58:37 -0600

    Sort deals by reverse chrono order (so we grab the latest, not just the first)

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,6 @@
 
 class DealsController < ApplicationController
   def index
-  	@deal_today = Deal.where(:deal_date => Date.today).first
+  	@deal_today = Deal.where(:deal_date => Date.today).order("created_at DESC").first
   end
 end
