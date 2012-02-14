0c106cde7ce90ac0db66ea3863155cdf1e302289
Matthew Ruten <matt@ruten.ca>
2012-02-14 17:24:02 -0500

    Add HTML email template

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,11 +1,11 @@
-class EmailController < ApplicationController
+class EmailsController < ApplicationController
 
   def text
 
   end
 
   def html
-    
+    render :partial => "emails/template_html.html.erb", :layout => false, :locals => {}
   end
 
 end
