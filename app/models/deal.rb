0b704abf0ecfb79d7894cf9811892722d429b88a
nomatteus <matt@ruten.ca>
2013-04-07 13:07:47 -0400

    Add links to all deals, and turn on affiliate links again, as they seem to be working now.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -6,11 +6,15 @@ class Deal < ActiveRecord::Base
     Deal.where(:deal_date => Date.today).order("created_at DESC").first
   end
 
+  def self.all_deals_link
+    "http://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000677541&tag=ruten-20"
+  end
+
   def affiliate_link
     # Oct 30, 2012: Removing tag from URL because it's causing 404s for some reason
     # Don't want to spend much time looking into this right now, so this is the quick fix
     if self.asin.blank?
-      self.link# + "&tag=ruten-20"
+      self.link + "&tag=ruten-20"
     else
       "http://www.amazon.com/dp/#{self.asin}/?tag=ruten-20"
     end
